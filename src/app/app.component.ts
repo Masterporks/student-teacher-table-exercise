ba2c7fc3c966b52b9e4351248ddfcdbc1d36313c
root <joosepkorela@gmail.com>
2023-02-08 21:35:39 +0200

    Students, teachers table separated to another component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,63 +11,18 @@ export class AppComponent implements OnInit {
   title = 'School-application';
 
   ngOnInit(): void {
-    this.students = this.getStudents();
-    this.teachers = this.getTeachers();
-
   }
 
 
   teachers: Teacher[] = [];
-  tFirstNames: string[] = ['Jackie', 'Lisa', 'Trevor'];
-  tLastNames: string[] = ['Looney', 'Fillor', 'Lambda'];
-  tSpecialities: string[] = ['Literature', 'Economy', 'Psychology'];
-
-  tAddresses: string[] = ['Narva', 'Viljandi', 'Finland'];
-  tEmails: string[] = ['looney@gmail.com', 'this@mail.ee', 'random@gmail.com'];
-
   students: Student[] = [];
-  sFirstNames: string[] = ['Millie', 'Laney', 'Tim'];
-  sLastNames: string[] = ['Hulk', 'Rock', 'Toon'];
-  sAges: number[] = [13, 16, 18];
-  sAddresses: string[] = ['Narva', 'Viljandi', 'Finland'];
-  sEmails: string[] = ['looy@gmail.com', 'is@mail.ee', 'rand@gmail.com'];
+studentTitle = 'List of students';
+teacherTitle = 'List of teachers';
 
-  getTeachers(): Teacher[] { //function return dog array
-    let teachers: Teacher[] = [];
-    teachers.push(new Teacher('Jack', 'Dobby', 'Science', 'Tallinn', 'dobby@gmail.com'));
-    teachers.push(new Teacher('Julie', 'Shepperd', 'Chemistry', 'Tartu', 'shepperd@gmail.com'));
 
-    return teachers;
-  }
 
-  getStudents(): Student[] { //function return dog array
-    let students: Student[] = [];
-    students.push(new Student('Jimmy', 'Doggie', 16, 'Tallinn', 'doggie@gmail.com'));
-    students.push(new Student('Julie', 'Sheep', 17, 'Tartu', 'sheep@gmail.com'));
 
-    return students;
-  }
 
-  addTeacher() {
-    let tFirstName = this.tFirstNames.at(Math.random()*this.tFirstNames.length);
-    let tLastName = this.tLastNames.at(Math.random()*this.tLastNames.length);
-    let tSpeciality = this.tSpecialities.at(Math.random()*this.tSpecialities.length);
-    let tAddress = this.tAddresses.at(Math.random()*this.tAddresses.length);
-    let tEmail = this.tEmails.at(Math.random()*this.tEmails.length);
 
-    // @ts-ignore
-    this.teachers.push(new Teacher(tFirstName,tLastName,tSpeciality,tAddress, tEmail));
-
-  }
-  addStudent() {
-    let sFirstName = this.sFirstNames.at(Math.random()*this.sFirstNames.length);
-    let sLastName = this.sLastNames.at(Math.random()*this.sLastNames.length);
-    let sAge = Math.floor(Math.random() *(+30 - +16)) + +16
-    let sAddress = this.sAddresses.at(Math.random()*this.sAddresses.length);
-    let sEmail = this.sEmails.at(Math.random()*this.sEmails.length);
-
-    // @ts-ignore
-    this.students.push(new Student(sFirstName,sLastName,sAge,sAddress, sEmail));
-  }
 
 }
---
+++ src/app/student/student.component.ts
@@ -0,0 +1,38 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Student} from "../shared/models/student";
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit {
+  @Input() studentTitle = "";
+  @Input() students: Student[] = [];
+  @Input() sFirstNames: string[] = ['Millie', 'Laney', 'Tim'];
+  @Input() sLastNames: string[] = ['Hulk', 'Rock', 'Toon'];
+  @Input() sAges: number[] = [13, 16, 18];
+  @Input() sAddresses: string[] = ['Narva', 'Viljandi', 'Finland'];
+  @Input() sEmails: string[] = ['looy@gmail.com', 'is@mail.ee', 'rand@gmail.com'];
+  ngOnInit(): void {
+    this.students = this.getStudents();
+  }
+  @Input() getStudents(): Student[] { //function return dog array
+    let students: Student[] = [];
+    students.push(new Student('Jimmy', 'Doggie', 16, 'Tallinn', 'doggie@gmail.com'));
+    students.push(new Student('Julie', 'Sheep', 17, 'Tartu', 'sheep@gmail.com'));
+
+    return students;
+  }
+  @Input() addStudent() {
+    let sFirstName = this.sFirstNames.at(Math.random()*this.sFirstNames.length);
+    let sLastName = this.sLastNames.at(Math.random()*this.sLastNames.length);
+    let sAge = Math.floor(Math.random() *(+35 - +16)) + +16
+    let sAddress = this.sAddresses.at(Math.random()*this.sAddresses.length);
+    let sEmail = this.sEmails.at(Math.random()*this.sEmails.length);
+
+    // @ts-ignore
+    this.students.push(new Student(sFirstName,sLastName,sAge,sAddress, sEmail));
+  }
+
+}
